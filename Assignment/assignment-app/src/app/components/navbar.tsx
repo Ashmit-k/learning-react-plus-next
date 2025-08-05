a8ddece3ec77c6c4400de441ca5be9475fcd3285
Sushilverma002 <sushilvarma9911@gmail.com>
2025-08-05 21:44:14 +0530

    formating changes

--- Assignment/assignment-app/src/app/components/navbar.tsx
+++ Assignment/assignment-app/src/app/components/navbar.tsx
@@ -1,58 +1,76 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import ButtonClass from '../utilities/buttons/button';
-import { ChevronDown, X, Menu } from 'lucide-react';
+import React, { useState } from "react";
+import ButtonClass from "../utilities/buttons/button";
+import { ChevronDown, X, Menu } from "lucide-react";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <>
+  return (
+    <>
+      {/* Fullscreen Mobile Menu */}
+      <div className={`mobileMenu ${isOpen ? "open" : ""}`}>
+        <div className="closeIcon" onClick={() => setIsOpen(false)}>
+          <X size={20} color="white" />
+        </div>
+        <ul>
+          <li>
+            <a href="#">Products</a>
+          </li>
+          <li>
+            <a href="#">Why Us?</a>
+          </li>
+          <li>
+            <a href="#">Plans</a>
+          </li>
+          <li>
+            <a href="#">For Developers</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <div className="navButtons">
+          <ButtonClass buttonText="Login" />
+        </div>
+      </div>
 
-            {/* Fullscreen Mobile Menu */}
-            <div className={`mobileMenu ${isOpen ? 'open' : ''}`}>
-                <div className="closeIcon" onClick={() => setIsOpen(false)}>
-                    <X size={20} color='white' />
-                </div>
-                <ul>
-                    <li><a href="#">Products</a></li>
-                    <li><a href="#">Why Us?</a></li>
-                    <li><a href="#">Plans</a></li>
-                    <li><a href="#">For Developers</a></li>
-                    <li><a href="#">Contact</a></li>
+      {/* Desktop Navbar */}
+      <div className="navbar">
+        <div className="logoDiv">
+          <img src="/assets/logo.png" alt="logo" />
+        </div>
+        {/* Hamburger Menu for Mobile */}
+        <div className="hamburger" onClick={() => setIsOpen(true)}>
+          <Menu size={24} color="white" />
+        </div>
 
-                </ul>
-                <div className="navButtons">
-                    <ButtonClass buttonText='Login' />
-                </div>
-            </div>
-
-            {/* Desktop Navbar */}
-            <div className="navbar">
-                <div className="logoDiv">
-                    <img src="/assets/logo.png" alt="logo" />
-                </div>
-                {/* Hamburger Menu for Mobile */}
-                <div className="hamburger" onClick={() => setIsOpen(true)}>
-                    <Menu size={24} color='white' />
-                </div>
-
-                <div className="navLinks">
-                    <ul>
-                        <li><a href="#">Products</a></li>
-                        <li><a href="#">Why Us?</a></li>
-                        <li><a href="#">Plans</a></li>
-                        <li><a href="#">For Developers</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-                <div className="navButtons">
-                    <ButtonClass buttonText='Login' />
-                </div>
-            </div>
-        </>
-    );
+        <div className="navLinks">
+          <ul>
+            <li>
+              <a href="#">Products</a>
+            </li>
+            <li>
+              <a href="#">Why Us?</a>
+            </li>
+            <li>
+              <a href="#">Plans</a>
+            </li>
+            <li>
+              <a href="#">For Developers</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navButtons">
+          <ButtonClass buttonText="Login" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
--- Assignment/assignment-app/src/app/page.tsx
+++ Assignment/assignment-app/src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import HeroSection from "./pages/heroSection";
 
-
 export default function Home() {
   return (
     <>
